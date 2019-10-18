2122e41402938337d0c7c8e422edf4908302855a
lujiayu666666 <1017575799@qq.com>
2019-10-19 00:42:21 +0800

    提交

--- src/mock/article.js
+++ src/mock/article.js
@@ -51,7 +51,7 @@ export default {
     source_name: '原创作者',
     category_item: [{ key: 'global', name: '全球' }],
     comment_disabled: true,
-    content: '<p>我是测试数据我是测试数据</p><p><img class="wscnph" src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943" data-wscntype="image" data-wscnh="300" data-wscnw="400" data-mce-src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>"',
+    content: '<p>我是测试数据我是测试数据啊</p><p><img class="wscnph" src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943" data-wscntype="image" data-wscnh="300" data-wscnw="400" data-mce-src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>"',
     content_short: '我是测试数据',
     display_time: +new Date(),
     image_uri: 'https://wpimg.wallstcn.com/e4558086-631c-425c-9430-56ffb46e70b3',
